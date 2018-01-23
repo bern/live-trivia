e109d8582b7b536186695b28677baab268ee9ce5
John Harrison <john.randolph.harrison@gmail.com>
2018-01-22 20:39:13 -0600

    routing, next adding redux to make it stateless/functional

--- trivia-frontend/src/App.js
+++ trivia-frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Streamer from './Streamer/Streamer';
 import Viewer from './Viewer/Viewer';
 import simulatedapi from './simulatedAsk'
@@ -12,12 +13,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <br/>
-        <Streamer></Streamer>
-        <br/>
-        <Viewer timer={simulatedapi.timer} question={simulatedapi.question} answers={simulatedapi.answers}></Viewer>
-      </div>
+      <Router>
+        <div className="App">
+          <Link to="/streamer">streamer component</Link>
+          <br/>
+          <Link to="/viewer"> viewer component</Link>
+
+          <Route path="/streamer" component={Streamer}/>
+
+          <Route path="/viewer" render={(props) => (<Viewer timer={simulatedapi.timer} question={simulatedapi.question} answers={simulatedapi.answers}/>)}/>
+
+          
+        </div>
+      </Router>
     );
   }
 }
